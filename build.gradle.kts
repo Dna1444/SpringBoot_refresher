8ca89af94e81f3fff40b9611279f13c71d180c3e
Dana Duquette <dana.duquette@fullharvest.com>
2025-07-07 14:13:07 -0400

    add google socket for postgres

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+	implementation("com.google.cloud.sql:postgres-socket-factory:1.6.2")
 }
 
 kotlin {
